886ba73cfe8a9d2720b893b631bad75264c963d7
Rensi Meila Yulvinata <rensimeyla04@gmail.com>
2025-03-10 01:33:04 +0700

    Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -105,7 +105,9 @@ class UserController extends Controller
         // $user->wasChanged('nama'); // false
         // dd($user->wasChanged(['nama', 'username'])); // true
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
