341af3a893de3f1665619a5287a568988338e7d8
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2025-04-13 21:25:14 +0700

    Implementasi File Export Excel

--- routes/web.php
+++ routes/web.php
@@ -107,6 +107,7 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
             Route::delete('/{id}', [BarangController::class, 'destroy']);
             Route::get('/import', [BarangController::class, 'import']);
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
+            Route::get('/export_excel', [BarangController::class, 'export_excel']);
         });
 
         Route::group(['prefix' => 'supplier'], function () {
